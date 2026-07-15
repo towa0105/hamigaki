b6912a04c17c4de62c88cf9673b634967f7d3dae
towa0105 <2250290@ecc.ac.jp>
2026-07-15 12:23:11 +0900

    追加：ページ遷移作成

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-const m
+const modal = document.querySelector(".modal")
+const playBtn = document.querySelector(".play_btn")
+const backBtn = document.querySelector(".back_btn")
+
+playBtn.addEventListener("click", () => {
+    modal.classList.toggle("active");
+    // if (!modal.classList.contains("active")) {
+    //     console.log("あああ");
+    //     modal.classList.add("active")
+    // } else {
+    //     modal.classList.remove("active")
+    // }
+})
+
+backBtn.addEventListener("click", () => {
+    modal.classList.toggle("active");
+})
+
