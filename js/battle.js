19650c51e46804ac3f4e70bf4170c02ee7515b0d
towa0105 <2250290@ecc.ac.jp>
2026-07-29 06:39:38 +0900

    追加：敵のHPゲージの増減

--- js/battle.js
+++ js/battle.js
@@ -1,19 +1,22 @@
 const boss = document.querySelector(".boss")
 const brush = document.querySelector(".brush")
 const modal = document.querySelector(".modal")
+const lifeBar = document.querySelector(".boss_gauge")
 let count = 0;
+let life = 100;
+lifeBar.style.width = "100%";
+
 
 brush.addEventListener("click", () => {
     console.log(1 + count++)
+    let Bar = (life -= 19);
+    console.log(Bar);
+    lifeBar.style.width = Bar + `%`;
+
+
     boss.classList.toggle("damage")
     if (count > 5) {
         boss.classList.add("active");
         modal.classList.add("active")
     }
 })
-
-
-// brush.addEventListener("click", () => {
-//     boss.classList.toggle("active");
-// })
-ー
